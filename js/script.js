c593aabd3e12aec6c2f91a67c97ea37e4778b96b
bashgahapp <49948830+bashgahapp@users.noreply.github.com>
2022-10-24 17:56:04 +0200

    5

--- js/script.js
+++ js/script.js
@@ -1,174 +1,171 @@
-//first: 
-var slideIndex = 1;         
-showDivs(slideIndex);
-
-function plusDivs(n) {
-  showDivs(slideIndex += n);
-}
-
-function showDivs(n) {
-  var i;
-  var x = document.getElementsByClassName("slide1");
-  if (n > x.length) {slideIndex = 1}    
-  if (n < 1) {slideIndex = x.length}
-  for (i = 0; i < x.length; i++) {
-     x[i].style.display = "none";  
-  }
-  x[slideIndex-1].style.display = "block";  
-}
-
-//--------------------------------------------------------------------
-                                      //second: 
-var slideIndex2 = 1;         
-showDivs2(slideIndex2);
-
-function plusDivs2(n) {
-  showDivs2(slideIndex2 += n);
-}
-
-function showDivs2(n) {
-  var i;
-  var x = document.getElementsByClassName("slide2");
-  if (n > x.length) {slideIndex2 = 1}    
-  if (n < 1) {slideIndex2 = x.length}
-  for (i = 0; i < x.length; i++) {
-     x[i].style.display = "none";  
-  }
-  x[slideIndex2-1].style.display = "block";  
-}
-//--------------------------------------------------------------------
-                                     //third: using w3.js library
-myslide = w3.slideshow(".slide3",0)
-//--------------------------------------------------------------------
-
-var slideIndex3 = 1;              //fourth
-showDivs3(slideIndex3);
-
-function plusDivs3(n) {
-  showDivs3(slideIndex3 += n);
-}
-
-function showDivs3(n) {
-  var i;
-  var x = document.getElementsByClassName("slide4");
-  if (n > x.length) {slideIndex3 = 1}    
-  if (n < 1) {slideIndex3 = x.length}
-  for (i = 0; i < x.length; i++) {
-     x[i].style.display = "none";  
-  }
-  x[slideIndex3-1].style.display = "block";  
-}
-
-
-//====================================================================
-var bag =[];                    // initialize price array   
-                  // Animation:
-$(document).ready(function(){
-  $('.cart').click(function(){
-    $('.checkout-container').fadeToggle();
-  });
+var ShoppingCart = (function($) {
+  "use strict";
   
-  $('.checkout').addClass('disabled');
-  $('#bin').addClass('disabled');
-});
-
-$(document).on('click','.add-to-cart',function(e){
-  e.preventDefault();
-  $('.empty').hide();
+  // Cahce necesarry DOM Elements
+  var productsEl = document.querySelector(".products"),
+      cartEl =     document.querySelector(".shopping-cart-list"),
+      productQuantityEl = document.querySelector(".product-quantity"),
+      emptyCartEl = document.querySelector(".empty-cart-btn"),
+      cartCheckoutEl = document.querySelector(".cart-checkout"),
+      totalPriceEl = document.querySelector(".total-price");
   
-  //---------------------------------------------------------
-  if($(this).hasClass('disable')){    //disable mutiple clicks
-    return false;
-  }
-  $(document).find('.add-to-cart').addClass('disable');
-  //---------------------------------------------------------
+  // Fake JSON data array here should be API call
+  var products = [
+    {
+      id: 0,
+      name: "iPhone 13",
+      description: "iPhone 13 features a cinema standard wide color gamut, displaying colors just as filmmakers intended.",
+      imageUrl: "./img/iphone-13.jpg",
+      price: 1199
+    },
+    {
+      id: 1,
+      name: "iPhone 13 Pro Max",
+      description: "Apple iPhone 13 Pro Max ; CPU, Hexa-core (2x3.22 GHz Avalanche + 4xX.X GHz Blizzard) ; GPU, Apple GPU (5-core graphics) ; Memory, Card slot ; Internal, 128GB 6GB",
+      imageUrl: "./img/iphone-pro-max.jpg",
+      price: 1999,
+    },
+    {
+      id: 2,
+      name: "Macbook Air",
+      description: "The M1 chip and macOS Monterey work together to make the entire system snappier. MacBook Air wakes instantly from sleep",
+      imageUrl: "./img/macbook-air.jpg",
+      price: 1499
+    },
+    {
+      id: 3,
+      name: "Macbook",
+      description: "The MacBook Air was among the first of Apple's Macs to make the transition to Apple silicon.",
+      imageUrl: "./img/macbook.jpg",
+      price: 999
+    },
+    {
+      id: 4,
+      name: "iPad 11inch",
+      description: "The iPad is Apple's most affordable and most popular tablet, and the ninth-generation model features the A13 Bionic chip.",
+      imageUrl: "./img/ipad.jpg",
+      price: 599
+    },
+    {
+      id: 5,
+      name: "iPad Mini",
+      description: "iPad is Apple's most affordable and most popular tablet, and the ninth-generation model features the A13 Bionic chip.",
+      imageUrl: "./img/ipad-mini.jpg",
+      price: 499
+    }
+  ],
+      productsInCart = [];
   
-  
-  
-  var parent = $(this).parents('.snip');
-  var src = parent.find('img').attr('src');
-  var cart = $(document).find('.cart');
-  
-  var posTop = parent.offset().top;    //return the coordinates of a element
-  var posLeft = parent.offset().left;
- 
-  $('<img />', { 
-   class: 'animation-fly',
-   src: src
-}).appendTo('body').css({
-    'top': posTop,      
-    'left': parseInt(posLeft)
-  }); 
-  
-  setTimeout(function(){
-    $(document).find('.animation-fly').css({
-      'top': cart.offset().top,
-      'left': cart.offset().left
+  // Pretty much self explanatory function. NOTE: Here I have used template strings (ES6 Feature)
+  var generateProductList = function() {
+    products.forEach(function(item) {
+      var productEl = document.createElement("div");
+      productEl.className = "product";
+      productEl.innerHTML = `<div class="product-image">
+                                <img src="${item.imageUrl}" alt="${item.name}">
+                             </div>
+                             <div class="product-name"><span>Product:</span> ${item.name}</div>
+                             <div class="product-description"><span>Description:</span> ${item.description}</div>
+                             <div class="product-price"><span>Price:</span> ${item.price} $</div>
+                             <div class="product-add-to-cart">
+                               <a href="#0" class="button see-more">More Details</a>
+                               <a href="#0" class="button add-to-cart" data-id=${item.id}>Add to Cart</a>
+                             </div>
+                          </div>
+`;
+                             
+productsEl.appendChild(productEl);
     });
-    setTimeout(function(){
-      $(document).find('.animation-fly').remove(); //after fly
-      var quantity = parseInt(cart.find('#count-item').data('count'))+1;
-//       if(quantity<2){
-//         cart.find('#count-item').text(quantity + ' item').data('count', quantity);
-//       }else{
-//         cart.find('#count-item').text(quantity + ' items').data('count', quantity);
-//       }
-      cart.find('#count-item').text(quantity + (quantity<2 ?' item':' items')).data('count', quantity);
-      
-                             //add item to cart
-    var name = parent.find('h4').text();
-    var price = parent.find('.real').text();
-    var txt3 = document.createElement("hr");
-    var txt4 = document.createElement("hr");
-       
-    $('.col1-name').append(name,txt3);
-    $('.col2-price').append(price,txt4);
-    $('.checkout').addClass('add-animation');
-    $('.checkout').removeClass('disabled');
+  }
+  
+  // Like one before and I have also used ES6 template strings
+  var generateCartList = function() {
     
-    $('#bin').addClass('add-animation2');
-    $('#bin').removeClass('disabled');
+    cartEl.innerHTML = "";
     
-                            //find total amount
-    var price2 = parseFloat(parent.find('.real').data('price')); //get "data-price" from <div class="real">
-    bag.push(price2);                                            
-    var total = 0;
-    $(".total-amount").text(function(){
-     for(var i in bag){
-      total += bag[i];    //calculate sum of all numbers in the array 
-     }
-     var last = "$ " + total.toFixed(2);
-     $('.pay-last').text(last);
-     return last;   // return total only -> bug.
-     
+    productsInCart.forEach(function(item) {
+      var li = document.createElement("li");
+      li.innerHTML = `${item.quantity} ${item.product.name} - $${item.product.price * item.quantity}`;
+      cartEl.appendChild(li);
     });
-      
-      $(document).find('.add-to-cart').removeClass('disable');
-    },1000);
-  },500);
+    
+    productQuantityEl.innerHTML = productsInCart.length;
+    
+    generateCartButtons()
+  }
   
-  $('.bin').on('click',function(){     
-   $('.col1-name').empty();
-   $('.col2-price').empty();
-   $('.empty').show();
-   $('.checkout').removeClass('add-animation');
-   $('#bin').removeClass('add-animation2');
-   $('.checkout').addClass('disabled');
-   $('#bin').addClass('disabled');
-   bag = [];
-   $('.total-amount').add('.pay-last').text("$ " + bag.length);
-   cart.find('#count-item').text(0 + ' item').data('count', 0);
- });
   
-});
+  // Function that generates Empty Cart and Checkout buttons based on condition that checks if productsInCart array is empty
+  var generateCartButtons = function() {
+    if(productsInCart.length > 0) {
+      emptyCartEl.style.display = "block";
+      cartCheckoutEl.style.display = "block"
+      totalPriceEl.innerHTML = "$ " + calculateTotalPrice();
+    } else {
+      emptyCartEl.style.display = "none";
+      cartCheckoutEl.style.display = "none";
+    }
+  }
+  
+  // Setting up listeners for click event on all products and Empty Cart button as well
+  var setupListeners = function() {
+    productsEl.addEventListener("click", function(event) {
+      var el = event.target;
+      if(el.classList.contains("add-to-cart")) {
+       var elId = el.dataset.id;
+       addToCart(elId);
+      }
+    });
+    
+    emptyCartEl.addEventListener("click", function(event) {
+      if(confirm("Are you sure?")) {
+        productsInCart = [];
+      }
+      generateCartList();
+    });
+  }
+  
+  // Adds new items or updates existing one in productsInCart array
+  var addToCart = function(id) {
+    var obj = products[id];
+    if(productsInCart.length === 0 || productFound(obj.id) === undefined) {
+      productsInCart.push({product: obj, quantity: 1});
+    } else {
+      productsInCart.forEach(function(item) {
+        if(item.product.id === obj.id) {
+          item.quantity++;
+        }
+      });
+    }
+    generateCartList();
+  }
+  
+  
+  // This function checks if project is already in productsInCart array
+  var productFound = function(productId) {
+    return productsInCart.find(function(item) {
+      return item.product.id === productId;
+    });
+  }
 
+  var calculateTotalPrice = function() {
+    return productsInCart.reduce(function(total, item) {
+      return total + (item.product.price *  item.quantity);
+    }, 0);
+  }
+  
+  // This functon starts the whole application
+  var init = function() {
+    generateProductList();
+    setupListeners();
+  }
+  
+  // Exposes just init function to public, everything else is private
+  return {
+    init: init
+  };
+  
+  // I have included jQuery although I haven't used it
+})(jQuery);
 
-
-
-//------------------BILL----------------------
-
-$(document).ready(function(){
-  $('#coupon').on('click',function(){
-    alert("minhquanghust.blogspot.com Coupon Code:" + '\n' + "25% off: 0511Q-1601CV" +'\n' + "15% off: 0511Q-1701NA" + '\n' + "10% off: 0511Q-1901QA")
-  });
-})
+ShoppingCart.init();
